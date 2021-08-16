0cd1b39213d208bbde0af24852c7010150e23b48
Archi-dev <gwsm14r@gmail.com>
2021-08-16 12:56:41 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+document.querySelector("#form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = {};
+  const inputs = event.target.querySelectorAll(" input, textarea, select");
+  for (let input of inputs) {
+    if (!(input.type === "checkbox") && !(input.name === "file")) {
+      data[input.name] = input.value;
+      }
+    // Сохраняем в обьект значение checkBox
+    if (input.type === "checkbox") {
+      data[input.name] = input.checked;
+      }
+    // Работаем с файлом(картинкой) и сразу в проверке убираем ошибку если файл не загружен
+    if (input.name === "file" && input.files.length === 1) {
+      // Позволяем асинхронно читать содержимое файлов, конвертирует файл в DataURL
+      const reader = new FileReader();
+      // событие при загрузке картинки
+      reader.addEventListener("load", () => {
+        //записываем в LocalStorage данные URL
+        localStorage.setItem("safeImage", reader.result);
+      });
+      // Используется для чтения содержимого файла
+      reader.readAsDataURL(input.files[0]);
+      }
+
+      }
+
+  localStorage.setItem("dataForm", JSON.stringify(data));
+      });
+    // Создаём событие при загрузке страници и записываем данные назад в инпуты c localStorage
+document.addEventListener("DOMContentLoaded", (ev) => {
+    // Если нужно при перезагрузке первой страници удалять вывод картинки на второй странице
+    //   localStorage.removeItem("safeImage");
+    //Останавливаем событие если в localStorage не записан обьект dataFprm.
+  if (!localStorage.dataForm) return;
+    //разбирает строку JSON
+  const data = JSON.parse(localStorage.getItem("dataForm"));
+    // собираем все инпуты в переменную
+  const inputs = document.querySelectorAll("#form input, textarea, select");
+    // перебираем элементы обьект
+  for (let input of inputs) {
+    // Если элемент не CheckBox и не файл
+    if (!(input.type === "checkbox") && !(input.name === "file")) {
+      input.value = data[input.name];
+    }
+    // Если элемент CheckBox
+    if (input.type === "checkbox") {
+      input.checked = data[input.name];
+    }
+    }
+    });
+
+    // localStorage.clear();
+
